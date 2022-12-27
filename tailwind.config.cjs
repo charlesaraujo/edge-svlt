ddd6ec09600ce8e8a180e797c5f8c86c74b32411
Charles Araújo <charles_araujo@ymail.com>
2022-12-27 08:20:28 -0300

    fix: acessibility on light theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,16 @@ module.exports = {
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
 		themes: [
-			'light',
 			{
 				dark: {
 					...require('daisyui/src/colors/themes')['[data-theme=dark]'],
 					secondary: '#BF409D'
+				}
+			},
+			{
+				light: {
+					...require('daisyui/src/colors/themes')['[data-theme=light]'],
+					secondary: '#D700AB'
 				}
 			}
 		]
